862c735c5b017608e6b77d6f1cbe1d094ccbc12b
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-01-28 12:48:22 -0500

    working on category-routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+const { response } = require('express');
+
+// The `/api/categories` endpoint
+
+
+// find all categories
+// be sure to include its associated Products
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,17 +4,25 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+
   // find all tags
   // be sure to include its associated Product data
+  res.send("Hit Get All Tags Route")
 });
 
 router.get('/:id', (req, res) => {
+  //console.log(req);
+  console.log(req.params);
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  res.send(`Found record for iem ${req.params.id}`)
 });
 
 router.post('/', (req, res) => {
+  console.log(req.body)
   // create a new tag
+  let temp = { name: req.body.name }
+  res.json(temp);
 });
 
 router.put('/:id', (req, res) => {
